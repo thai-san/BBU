39f4af6f5004d5920ffed82ddb4abc8c8b6508b6
lypo <peacelypo@gmail.com>
2014-05-01 17:29:10 +0700

    add home page, admin controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "student";
+$route['default_controller'] = "home";
 $route['404_override'] = '';
 
 
---
+++ application/controllers/admin.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file admin.php */
+/* Location: ./application/controllers/admin.php */
